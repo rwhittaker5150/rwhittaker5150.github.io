e94015554aa16c6611bdc8f33e67ee0f5030cd90
Richard <rwhittaker5150@gmail.com>
2018-12-14 15:22:27 -0500

    Created Search page Added Destinations Added Images

---
+++ app/src/main/java/twu/whittaker/search/CustomAdapter.java
@@ -0,0 +1,47 @@
+package twu.whittaker.search;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import twu.whittaker.R;
+
+public class CustomAdapter extends BaseAdapter {
+    private Context context;
+    private final String[] countryList;
+    private final int[] flags;
+    private final LayoutInflater inflater;
+
+    public CustomAdapter(Context applicationContext, String[] countryList, int[] flags){
+        this.countryList = countryList;
+        this.flags = flags;
+        inflater = (LayoutInflater.from(applicationContext));
+    }
+
+    @Override
+    public int getCount(){
+        return countryList.length;
+    }
+    @Override
+    public Object getItem(int i){
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i){
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup){
+        view = inflater.inflate(R.layout.activity_listview, null);
+        TextView country = view.findViewById(R.id.textView);
+        ImageView icon = view.findViewById(R.id.icon);
+        country.setText(countryList[i]);
+        icon.setImageResource(flags[i]);
+        return view;
+    }
+}
--- app/src/main/java/twu/whittaker/search/SearchActivity.java
+++ app/src/main/java/twu/whittaker/search/SearchActivity.java
@@ -1,16 +1,24 @@
 package twu.whittaker.search;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 import twu.whittaker.R;
 
-
 public class SearchActivity extends AppCompatActivity {
+
+    ListView listView;
+    String countryList[] = {"India", "China", "Australia", "Portugle", "America", "New Zealand"};
+    int flags[] = {R.drawable.india, R.drawable.china, R.drawable.australia, R.drawable.portugle, R.drawable.america, R.drawable.newzealand};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        setContentView(R.layout.activity_search);
+        listView = findViewById(R.id.destListView);
+        CustomAdapter customAdapter = new CustomAdapter(getApplication(), countryList, flags);
+        listView.setAdapter(customAdapter);
     }
 }
